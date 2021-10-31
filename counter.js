a16a22dd3731ab5f0891126720026ffcb798b1a6
Sevrinn <sevrinnsevrinn@gmail.com>
2021-10-31 15:33:50 -0700

    inital commit

---
+++ counter.js
@@ -0,0 +1,9 @@
+/**
+ * Passenger Counter
+ * This app is meant to help count and record number of passengers entering a subway station.
+ * 
+ */
+
+let count = 0
+let counter = document.getElementById("count-el")
+counter.innerText = count;
